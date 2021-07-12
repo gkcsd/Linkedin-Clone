47b147396130dbb85cc99494d4532d9752d0923d
gkcsd <kaleg2025@gmail.com>
2021-07-12 22:12:05 +0530

    Feed comp added

---
+++ src/Feed.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Feed.css";
+
+const Feed = () => {
+    return(
+        <div className="feed">
+            <h1>This is Feed</h1>
+        </div>
+    )
+}
+export default Feed;
