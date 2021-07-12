377b0b927e9079c2e521e95ee79b89b59c31abf0
gkcsd <kaleg2025@gmail.com>
2021-07-12 15:26:54 +0530

    sidebar added in app body

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
-      {/* Header */}
       <Header />
 
       {/* App Body */}
-        {/* sidebar */}
+      <div className="app_body">
+        <Sidebar />
+
         {/* feed */}
         {/* widgets */}
+      </div>
 
     </div>
   );
