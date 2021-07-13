951a578d4eb95aba67fbbcc9abfbaa7184f0c8db
gkcsd <kaleg2025@gmail.com>
2021-07-13 13:36:09 +0530

    Feed section added

--- src/Feed.js
+++ src/Feed.js
@@ -1,11 +1,44 @@
 import React from "react";
 import "./Feed.css";
+import CreateIcon from "@material-ui/icons/Create";
+import InputOption from "./InputOption";
+import ImageIcon from "@material-ui/icons/Image"
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote"
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay"
+import Post from "./Post";
 
 const Feed = () => {
     return(
         <div className="feed">
-            <h1>This is Feed</h1>
+            <div className="feed_inputContainer">
+                <div className="feed_input">
+                    <CreateIcon />
+                    <form>
+                        <input type="text" placeholder="Start a post" />
+                        <button type="submit">Send</button>
+                    </form>
+                </div>
+                <div className="feed_inputOptions">
+                    <InputOption Icon={ImageIcon} title="Photo" color="#7085F9" />
+                    <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+                    <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+                    <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E" />
+                </div>
+            </div>
+
+            {/* Post */}
+            <Post name="Ganesh Kale" description="This is a test"
+            message="Wow, this worked"
+            />
+
         </div>
     )
 }
 export default Feed;
+
+
+
+
+
+
