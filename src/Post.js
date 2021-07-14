aa0cda29869c3c861babc7218d93ee8caad2031c
gkcsd <kaleg2025@gmail.com>
2021-07-14 22:29:23 +0530

    post para for destructuring added

--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,11 @@
 import {Avatar} from "@material-ui/core";
 import React from "react";
+import InputOption from "./InputOption";
 import "./Post.css";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined"
+import ChatOutlinedIcon from "@material-ui/icons/ChatOutlined";
+import FolderSharedOutlinedIcon from "@material-ui/icons/FolderSharedOutlined"
+import SendOutlinedIcon from "@material-ui/icons/SendOutlined"
 
 const Post = ({name, description, message, photoUrl}) => {
     return(
@@ -8,13 +13,32 @@ const Post = ({name, description, message, photoUrl}) => {
             <div className="post_header">
                 <Avatar />
                 <div className="post_info">
-                    <h2>Ganesh Kale</h2>
-                    <p>Description</p>
+                    <h2>{name}</h2>
+                    <p>{description}</p>
                 </div>
             </div>
 
             <div className="post_body">
-                <p>Message goes here</p>
+                <p>{message}</p>
+            </div>
+
+            <div className="post_buttons">
+                <InputOption className="post_button" Icon={ThumbUpAltOutlinedIcon} 
+                title="Like"
+                color="gray"
+                />
+                <InputOption className="post_button" Icon={ChatOutlinedIcon} 
+                title="Comment"
+                color="gray"
+                />
+                <InputOption className="post_button" Icon={FolderSharedOutlinedIcon} 
+                title="Share"
+                color="gray"
+                />
+                <InputOption className="post_button" Icon={SendOutlinedIcon} 
+                title="Send"
+                color="gray"
+                />
             </div>
 
         </div>
