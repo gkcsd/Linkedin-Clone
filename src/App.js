0fbd887941cdfdcd97ccb4fe319d587d3695d8bb
gkcsd <kaleg2025@gmail.com>
2021-07-15 13:49:04 +0530

    redux prop and conditional rendering added

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from "./Feed";
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import Login from "./Login";
 
-function App() {
+const App = () => {
+
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
 
-      {/* App Body */}
-      <div className="app_body">
-        <Sidebar />
-        <Feed />
-      
-        {/* widgets */}
-      </div>
-
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app_body">
+          <Sidebar />
+          <Feed />
+        
+          {/* widgets */}
+        </div>
+      )}
     </div>
   );
 }
