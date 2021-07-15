8d0e298255fcedc7419d61d01feb70bde34aa17b
gkcsd <kaleg2025@gmail.com>
2021-07-15 13:49:55 +0530

    img and form added

---
+++ src/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Login.css";
+
+const Login = () => {
+    return(
+        <div className="login">
+            <img
+                src="https://news.hitb.org/sites/default/files/styles/large/public//field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks"
+                alt=""
+            />
+            <form>
+                <input type="text" placeholder="Full name ( required if registering)" />
+            </form>
+        </div>
+    )
+}
+export default Login;
