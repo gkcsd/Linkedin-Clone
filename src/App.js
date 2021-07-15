ae72859ef1b2bd0c1b39f6171339b796d8b16651
gkcsd <kaleg2025@gmail.com>
2021-07-15 15:16:06 +0530

    useEffect added for userAuth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from "./Feed";
-import { useSelector } from 'react-redux';
-import { selectUser } from './features/userSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import { auth } from './firebase';
 import Login from "./Login";
 
 const App = () => {
 
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect((userAuth) => {
+    if(userAuth) {
+      //user is loged in
+      dispatch(
+        login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoUrl: userAuth.photoURL,
+        })
+      )
+    } else {
+      //user is logged out
+      dispatch(logout());
+    }
+  }, [])
 
   return (
     <div className="app">
