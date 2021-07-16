a4bd30d8dbb69521d1dd5393b7b665a386d742a7
gkcsd <kaleg2025@gmail.com>
2021-07-16 22:55:55 +0530

    conditions added

--- src/Header.js
+++ src/Header.js
@@ -40,7 +40,7 @@ const Header = () => {
                 <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
                 <HeaderOption Icon={ChatIcon} title="Messaging" />
                 <HeaderOption Icon={NotificationsIcon} title="Notifications" />
-                <HeaderOption avatar={user ? true : false} title={user?.displayName}
+                <HeaderOption avatar={user ? true : false} title={!(user?.displayName) ? (user ? "Me" : "") : user?.displayName}
                 onClick={logoutOfApp}
                 />
             </div>
