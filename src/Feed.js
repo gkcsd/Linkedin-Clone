0b486302f6809719d4041701c04daeb04406d88f
gkcsd <kaleg2025@gmail.com>
2021-07-16 20:12:21 +0530

    useSelector state added

--- src/Feed.js
+++ src/Feed.js
@@ -9,8 +9,13 @@ import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay"
 import Post from "./Post";
 import { db } from "./firebase";
 import firebase from "firebase";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import FlipMove from "react-flip-move";
 
 const Feed = () => {
+
+    const user = useSelector(selectUser);
 
     const [input, setInput] = useState('');
     const [posts, setPosts] = useState([]);
@@ -30,10 +35,10 @@ const Feed = () => {
         e.preventDefault();
 
         db.collection("posts").add({
-            name: "Ganesh Kale",
-            description: "This is test",
+            name: user.displayName,
+            description: user.email,
             message: input,
-            photoUrl: "",
+            photoUrl: user.photoUrl || "",
             timestamp: firebase.firestore.FieldValue.serverTimestamp(),
         });
 
@@ -60,7 +65,8 @@ const Feed = () => {
 
             {/* Post */}
 
-            {posts.map(({id, data: { name, description, message, 
+           <FlipMove>
+           {posts.map(({id, data: { name, description, message, 
             photoUrl } }) => (
                 <Post 
                 key={id}
@@ -70,6 +76,7 @@ const Feed = () => {
                 photoUrl={photoUrl}
                 />
             ))}
+           </FlipMove>
 
         </div>
     )
