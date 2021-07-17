1bd97bfd684b53e321e5f21678f82e759c56a624
gkcsd <kaleg2025@gmail.com>
2021-07-17 17:50:59 +0530

    Sidebar comp path changed

---
+++ src/components/Body/Sidebars/Sidebar.js
@@ -0,0 +1,53 @@
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../../features/userSlice";
+import "../Sidebars/Sidebar.css";
+
+const Sidebar = () => {
+
+    const user = useSelector(selectUser);
+
+    const recentItem = (topic) => (
+        <div className="sidebar_recentItem">
+            <span className="sidebar_hash">#</span>
+            <p>{topic}</p>
+        </div>
+    )
+
+    return(
+        <div className="sidebar">
+            <div className="sidebar_top">
+                <img src="https://images.unsplash.com/photo-1625474941653-4343bbb8267a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80" alt="" />
+                <Avatar src={user.photoUrl} className="sidebar_avatar">{user.email[0]}</Avatar>
+                <h2>{user.displayName}</h2>
+                <h4>{user.email}</h4>
+            </div>
+            <div className="sidebar_stats">
+                <div className="sidebar_stat">
+                    <p>Who viewed your profile</p>
+                    <p className="sidebar_statNumber">
+                        48
+                    </p>
+                </div>
+                <div className="sidebar_stat">
+                <p>Views of your post</p>
+                    <p className="sidebar_statNumber">
+                        334
+                    </p>
+                </div>
+            </div>
+
+            <div className="sidebar_bottom">
+                <p>Recent</p>
+                {recentItem('reactjs')}
+                {recentItem('nodejs')}
+                {recentItem('java')}
+                {recentItem('systemdesign')}
+                {recentItem('programming')}
+                {recentItem('developers')}
+            </div>
+        </div>
+    )
+}
+export default Sidebar;
